911f850e3c1199aafe50876be886856d68e8ed07
DaanishHindustaniTR <daanishhindustani@yahoo.com>
2025-06-21 11:40:50 -0500

    feat: Inital Commit for boiler plate code

---
+++ quickrecapai/src/main/java/com/recapvidai/backendquickrecapai/QuickrecapaiApplication.java
@@ -0,0 +1,13 @@
+package com.recapvidai.backendquickrecapai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickrecapaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickrecapaiApplication.class, args);
+	}
+
+}
---
+++ quickrecapai/src/test/java/com/recapvidai/backendquickrecapai/QuickrecapaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recapvidai.backendquickrecapai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickrecapaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
