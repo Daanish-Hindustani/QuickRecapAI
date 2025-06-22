76d1581f7b0963724aa2be34e43d4fc87606ae09
DaanishHindustaniTR <daanishhindustani@yahoo.com>
2025-06-22 14:54:25 -0500

    feat: added Video recapped service.

---
+++ quickrecapai/src/main/java/com/recapvidai/backendquickrecapai/service/RecappedVideoService.java
@@ -0,0 +1,10 @@
+package com.recapvidai.backendquickrecapai.service;
+
+
+import com.recapvidai.backendquickrecapai.model.RecappedVideoModel;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecappedVideoService {
+
+}
